69d4ec632ff0f03bfdf3e034d6e8b94e50517850
Hendrick Manullang <h.manullang@gmail.com>
2022-06-12 23:27:52 +1000

    refactored

--- rock-paper-scissors/rockPaperScissors.js
+++ rock-paper-scissors/rockPaperScissors.js
@@ -18,6 +18,7 @@ let computerScore = 0;
 // play a game of rock paper scissors
 function play(playerChoice, computerChoice) {
   computerChoice = computerPlay();
+
   // prompt user for rock, paper, or scissors
   playerChoice = prompt('Rock, Paper, or Scissors? > ').toLowerCase();
 
@@ -33,7 +34,7 @@ function play(playerChoice, computerChoice) {
     if (playerChoice === 'rock') {
       if (computerChoice === 'scissors') {
         yourScore ++;
-        return `You win! Your score ${yourScore}, computer score: ${computerScore}`;
+        return `You win! Your score ${yourScore}, computer score: ${computerScore}`;;
       } else {
         computerScore ++;
         return `You lose! Your score ${yourScore}, computer score: ${computerScore}`;
@@ -44,7 +45,7 @@ function play(playerChoice, computerChoice) {
     if (playerChoice === 'paper') {
       if (computerChoice === 'rock') {
         yourScore ++;
-        return `You win! Your score ${yourScore}, computer score: ${computerScore}`;
+        return `You win! Your score ${yourScore}, computer score: ${computerScore}`;;
       } else {
         computerScore ++;
         return `You lose! Your score ${yourScore}, computer score: ${computerScore}`;
@@ -55,7 +56,7 @@ function play(playerChoice, computerChoice) {
     if (playerChoice === 'scissors') {
       if (computerChoice === 'paper') {
         yourScore ++;
-        return `You win! Your score ${yourScore}, computer score: ${computerScore}`;
+        return `You win! Your score ${yourScore}, computer score: ${computerScore}`;;
       } else {
         computerScore ++;
         return `You lose! Your score ${yourScore}, computer score: ${computerScore}`;
@@ -66,6 +67,7 @@ function play(playerChoice, computerChoice) {
 while (
   yourScore < Math.round(gameLength/2) &&
   computerScore < Math.round(gameLength/2)
-  ) {console.log(play(playerChoice, computerChoice));}
+  ) {console.log(play(playerChoice, computerChoice));
+}
 
 module.exports = play;
