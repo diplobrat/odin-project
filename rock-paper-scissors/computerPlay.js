634a3c1a3a944e1842a3a435772178d937dc0f08
Hendrick Manullang <h.manullang@gmail.com>
2022-06-23 18:55:02 +1000

    work in progress

--- rock-paper-scissors/computerPlay.js
+++ rock-paper-scissors/computerPlay.js
@@ -4,4 +4,7 @@ function computerPlay() {
   return choices[Math.floor(Math.random()*choices.length)];
 };
 
+// to do: introduce a "predict" function that takes the history of the player's choice
+// and produces an output which is most likely to beat the player's next choice
+
 module.exports = computerPlay;
